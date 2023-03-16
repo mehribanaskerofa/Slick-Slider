59677f5f3454d8161d71411075aac7fd6b0bb597
mehribanaskerova <74506433+mehribanaskerova@users.noreply.github.com>
2023-03-16 23:54:43 +0400

    Slick Slider

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.fashion-slider').slick({
+      dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    slidesToShow: 1,
+    adaptiveHeight: true
+    });
+    });
