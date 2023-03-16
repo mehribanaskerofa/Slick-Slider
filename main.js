d635a2a91b086979a02595c0aad169c43ddef73d
mehribanaskerova <74506433+mehribanaskerova@users.noreply.github.com>
2023-03-17 00:41:24 +0400

    another slider

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// fashion slider----------------------------------------
 $(document).ready(function(){
     $('.fashion-slider').slick({
       dots: true,
@@ -9,3 +10,43 @@ $(document).ready(function(){
     adaptiveHeight: true
     });
     });
+//=======================================================
+
+
+//product-slider
+// $(document).ready(function(){
+//       $('.product-slider').slick({
+//         // dots: true,
+//       infinite: true,
+//       speed: 300,
+//       autoplay: true,
+//       autoplaySpeed: 3500,
+//       slidesToShow: 4,
+//       adaptiveHeight: true,
+//       responsive: [
+//         {
+//           breakpoint: 1200,
+//           settings: {
+//             slidesToShow: 3,
+//             slidesToScroll: 3,
+//             infinite: true,
+//             dots: true
+//           }
+//         },
+//         {
+//           breakpoint: 900,
+//           settings: {
+//             slidesToShow: 2,
+//             slidesToScroll: 2
+//           }
+//         },
+//         {
+//           breakpoint: 750,
+//           settings: {
+//             slidesToShow: 1,
+//             slidesToScroll: 1
+//           }
+//         }
+//       ]
+//       });
+//       });
